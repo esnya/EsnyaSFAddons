0ffa580bafa608a9861a9d5128057d7786c4863d
esnya <esnya@nekometer.com>
2021-12-21 18:58:04 +0900

    :sparkles: feat: WindChanger Sync

---
+++ Assets/EsnyaSFAddons/Scripts/WindChanger_SyncTrigger.cs
@@ -0,0 +1,26 @@
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+
+namespace EsnyaFactory
+{
+
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    [RequireComponent(typeof(VRC_Pickup))]
+    [RequireComponent(typeof(VRCObjectSync))]
+    public class WindChanger_SyncTrigger : UdonSharpBehaviour
+    {
+        public override void OnPickupUseDown()
+        {
+            GetComponentInChildren<WindChanger_Sync>()._Sync();
+        }
+
+#if !COMPILER_UDONSHARP && UNITY_EDITOR
+        private void Reset()
+        {
+            GetComponent<VRCObjectSync>().AllowCollisionOwnershipTransfer = false;
+        }
+#endif
+    }
+}

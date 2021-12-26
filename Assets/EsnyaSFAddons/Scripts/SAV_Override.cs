a9b1745b1e707589bd2e1358925bcf4254daeef3
esnya <esnya@nekometer.com>
2021-12-27 00:43:55 +0900

    :sparkles: feat: SAV_Override

---
+++ Assets/EsnyaSFAddons/Scripts/SAV_Override.cs
@@ -0,0 +1,46 @@
+using UdonSharp;
+using UnityEngine;
+
+namespace EsnyaAircraftAssets {
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class SAV_Override : UdonSharpBehaviour
+    {
+        [Header("Health")]
+        public bool overrideHealth;
+        public float healthMultiplier = 1;
+
+        [Header("Fuel")]
+        public bool overrideFuel;
+        public float fuelMultiplier = 1;
+
+        [Header("Brake")]
+        public bool overrideBrake;
+        public float airbrakeMultiplier = 1;
+        public float groundBrakeMultiplier = 1;
+        public float waterBrakeMultiplier = 1;
+
+        public void SFEXT_L_EntityStart()
+        {
+            SendCustomEventDelayedSeconds(nameof(_LateStart), 3);
+        }
+
+        public void _LateStart()
+        {
+            var entity = GetComponentInParent<SaccEntity>();
+            var airVehicle = GetComponentInChildren<SaccAirVehicle>(true);
+
+            if (overrideHealth) airVehicle.SetProgramVariable("FullHealth", ((float)airVehicle.GetProgramVariable("FullHealth")) * healthMultiplier);
+            if (overrideFuel) airVehicle.SetProgramVariable("FullFuel", ((float)airVehicle.GetProgramVariable("FullFuel")) * fuelMultiplier);
+
+            if (overrideBrake)
+            {
+                var brake = entity.GetComponentInChildren<DFUNC_Brake>(true);
+                brake.AirbrakeStrength *= airbrakeMultiplier;
+                brake.GroundBrakeStrength *= groundBrakeMultiplier;
+                brake.WaterBrakeStrength *= waterBrakeMultiplier;
+            }
+
+            gameObject.SetActive(false);
+        }
+    }
+}

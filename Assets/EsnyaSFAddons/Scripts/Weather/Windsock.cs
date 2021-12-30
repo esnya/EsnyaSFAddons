53c93bb48f12fb194eaebc90f308302689bc7a6e
esnya <esnya@nekometer.com>
2021-12-31 04:15:02 +0900

    :sparkles: feat(windwosk): Apply gust

--- Assets/EsnyaSFAddons/Scripts/Weather/Windsock.cs
+++ Assets/EsnyaSFAddons/Scripts/Weather/Windsock.cs
@@ -12,7 +12,7 @@ namespace EsnyaAircraftAssets
     public class Windsock : UdonSharpBehaviour
     {
         public SAV_WindChanger windChanger;
-        public int updateInterval = 90;
+        public int updateInterval = 30;
         public float maxWindMagnitude = 100.0f;
         public float rotationSmooth = 100.0f;
 
@@ -34,13 +34,20 @@ namespace EsnyaAircraftAssets
             if ((Time.frameCount + updateOffset) % updateInterval != 0 || windChanger == null) return;
 
             var wind = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).Wind;
+            var windGustiness = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindGustiness;
+            var windTurbulanceScale = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindTurbulanceScale;
+            var windGustStrength = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindGustStrength;
 
-            heading = Mathf.LerpAngle(heading, Vector3.SignedAngle(Vector3.forward, Vector3.ProjectOnPlane(wind, Vector3.up), Vector3.up), rotationSmooth * Mathf.Clamp01(wind.magnitude) / (Time.deltaTime * updateInterval));
+            var timeGustiness = Time.time * windGustiness;
+            var gustx = timeGustiness + (transform.position.x * windTurbulanceScale);
+            var gustz = timeGustiness + (transform.position.z * windTurbulanceScale);
+            var finalWind = wind + Vector3.Normalize(new Vector3(Mathf.PerlinNoise(gustx + 9000, gustz) - .5f, 0, Mathf.PerlinNoise(gustx, gustz + 9999) - .5f)) * windGustStrength;
 
+            heading = Mathf.LerpAngle(heading, Vector3.SignedAngle(Vector3.forward, Vector3.ProjectOnPlane(finalWind, Vector3.up), Vector3.up), rotationSmooth * Mathf.Clamp01(wind.magnitude) / (Time.deltaTime * updateInterval));
             transform.rotation = Quaternion.AngleAxis(heading, Vector3.up) * initialRotation;
 
-            animator.SetFloat("x", Mathf.Clamp(wind.x / maxWindMagnitude, -1.0f, 1.0f));
-            animator.SetFloat("z", Mathf.Clamp(wind.z / maxWindMagnitude, -1.0f, 1.0f));
+            animator.SetFloat("x", Mathf.Clamp(finalWind.x / maxWindMagnitude, -1.0f, 1.0f));
+            animator.SetFloat("z", Mathf.Clamp(finalWind.z / maxWindMagnitude, -1.0f, 1.0f));
         }
     }
 }

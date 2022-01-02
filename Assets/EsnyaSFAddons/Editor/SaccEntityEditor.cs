7722f3a16ed333d2d2dd465f4791723a1a14c73d
esnya <esnya@nekometer.com>
2022-01-02 17:24:20 +0900

    :recycle: refactor: Update Template Structure

--- Assets/EsnyaSFAddons/Editor/SaccEntityEditor.cs
+++ Assets/EsnyaSFAddons/Editor/SaccEntityEditor.cs
@@ -7,6 +7,8 @@ using UdonSharp.Serialization;
 using UdonSharpEditor;
 using UnityEditor;
 using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
 using VRC.Udon.Graph.NodeRegistries;
 
 namespace EsnyaAircraftAssets
@@ -20,7 +22,7 @@ namespace EsnyaAircraftAssets
         }
         private static bool BigButton(string label)
         {
-            return GUILayout.Button(label, new [] { GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(true) });
+            return GUILayout.Button(label, new[] { GUILayout.ExpandWidth(false), GUILayout.ExpandHeight(true) });
         }
 
         private static void SetObjectArrayProperty<T>(SerializedProperty property, IEnumerable<T> enumerable) where T : UnityEngine.Object
@@ -36,8 +38,11 @@ namespace EsnyaAircraftAssets
 
         private static bool HelpBoxWithAutoFix(string message, MessageType messageType)
         {
-            EditorGUILayout.HelpBox(message, messageType);
-            return BigButton("Auto Fix");
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUILayout.HelpBox(message, messageType);
+                return BigButton("Auto Fix");
+            }
         }
 
         private static void UndoRecordUdonSharpBehaviour(UdonSharpBehaviour udonSharpBehaviour, string name)
@@ -125,6 +130,59 @@ namespace EsnyaAircraftAssets
             }
         }
 
+        private static void AlignMFDFunctions(SaccEntity entity, VRC_Pickup.PickupHand side)
+        {
+            var parent = entity.InVehicleOnly?.transform ?? entity.transform;
+            var display = FindByName(parent, $"StickDisplay{side.ToString()[0]}")?.transform;
+            if (!display) return;
+
+            var functions = Enumerable
+                .Range(0, display.childCount)
+                .Select(display.GetChild)
+                .Where(t => t.gameObject.name.StartsWith("MFD_"))
+                .Select((transform, index) => (transform, index))
+                .ToArray();
+
+            var count = functions.Length;
+            var dialFunctions = side == VRC_Pickup.PickupHand.Left ? entity.Dial_Functions_L : entity.Dial_Functions_R;
+            foreach (var (transform, index) in functions)
+            {
+                var localRotation = Quaternion.AngleAxis(360.0f * index / count, Vector3.back);
+                var localPosition = localRotation * Vector3.up * 0.14f;
+
+                Undo.RecordObject(transform, "Align MFD Function");
+                transform.localPosition = localPosition;
+
+                var dialFunction = dialFunctions != null && index < dialFunctions.Length ? dialFunctions[index] : null;
+
+                var displayHighlighter = transform.Find("MFD_display_funcon")?.gameObject;
+                if (displayHighlighter)
+                {
+                    Undo.RecordObject(displayHighlighter.transform, "Align MFD Function");
+                    displayHighlighter.transform.position = transform.parent.position;
+                    displayHighlighter.transform.localRotation = localRotation;
+
+                    if ((UnityEngine.Object)dialFunction.GetProgramVariable("Dial_Funcon") != displayHighlighter)
+                    {
+                        var udon = UdonSharpEditorUtility.GetBackingUdonBehaviour(dialFunction);
+                        Undo.RecordObject(udon, "Align MFD Function");
+                        dialFunction.SetProgramVariable("Dial_Funcon", displayHighlighter);
+                        dialFunction.ApplyProxyModifications();
+                        EditorUtility.SetDirty(udon);
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<GameObject> ListByName(Transform root, string name)
+        {
+            return root.GetComponentsInChildren<Transform>(true).OrderBy(t => t.GetHierarchyPath().Count(c => c == '/')).Select(t => t.gameObject).Where(o => o.name == name);
+        }
+        private static GameObject FindByName(Transform root, string name)
+        {
+            return ListByName(root, name).FirstOrDefault();
+        }
+
         public override void OnInspectorGUI()
         {
             if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(target)) return;
@@ -150,10 +208,28 @@ namespace EsnyaAircraftAssets
                     else if (property.name == nameof(SaccEntity.Dial_Functions_L))
                     {
                         if (MiniButton("Find")) SetObjectArrayProperty(property, SFUtils.FindDFUNCs(entity.gameObject).Where(dfunc => dfunc.transform.parent.gameObject.name.EndsWith("L")));
+                        if (MiniButton("Align")) AlignMFDFunctions(entity, VRC_Pickup.PickupHand.Left);
                     }
                     else if (property.name == nameof(SaccEntity.Dial_Functions_R))
                     {
                         if (MiniButton("Find")) SetObjectArrayProperty(property, SFUtils.FindDFUNCs(entity.gameObject).Where(dfunc => dfunc.transform.parent.gameObject.name.EndsWith("R")));
+                        if (MiniButton("Align")) AlignMFDFunctions(entity, VRC_Pickup.PickupHand.Right);
+                    }
+                    else if (property.name == nameof(SaccEntity.InVehicleOnly) || property.name == nameof(SaccEntity.HoldingOnly))
+                    {
+                        if (MiniButton("Find")) property.objectReferenceValue = FindByName(entity.transform, property.name);
+                    }
+                    else if (property.name == nameof(SaccEntity.CenterOfMass) || property.name == nameof(SaccEntity.LStickDisplayHighlighter) || property.name == nameof(SaccEntity.RStickDisplayHighlighter))
+                    {
+                        if (MiniButton("Find")) property.objectReferenceValue = FindByName(entity.transform, property.name)?.transform;
+                    }
+                    else if (property.name == nameof(SaccEntity.SwitchFunctionSound))
+                    {
+                        if (MiniButton("Find")) property.objectReferenceValue = ListByName(entity.transform, property.name).Select(o => o.GetComponent<AudioSource>()).FirstOrDefault();
+                    }
+                    else if (property.name == nameof(SaccEntity.DisableAfter10Seconds))
+                    {
+                        if (MiniButton("Find")) SetObjectArrayProperty(property, ListByName(entity.transform, property.name));
                     }
                 }
             }

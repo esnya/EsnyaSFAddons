f586e4225d60c6827b1b5c2ba7d1625ab03fdc2a
esnya <esnya@nekometer.com>
2022-01-13 15:18:27 +0900

    :recycle: refactor(editor): Refactor editor utilities

--- Assets/EsnyaSFAddons/Editor/SFUtils.cs
+++ Assets/EsnyaSFAddons/Editor/SFUtils.cs
@@ -1,17 +1,16 @@
-using System.Collections;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UdonSharp;
-using UdonSharp.Serialization;
 using UdonSharpEditor;
 using UnityEditor;
 using UnityEngine;
+using VRC.SDKBase;
 
 namespace EsnyaAircraftAssets
 {
-    public class SFUtils
+    public static class SFUtils
     {
         public static IEnumerable<FieldInfo> ListPublicVariables(Type type)
         {
@@ -35,6 +34,7 @@ namespace EsnyaAircraftAssets
         {
             return ListCustomEvents(type).Any(m => m.Name.StartsWith("DFUNC_"));
         }
+
         public static IEnumerable<UdonSharpBehaviour> FindExtentions(GameObject root)
         {
             return root.GetUdonSharpComponentsInChildren<UdonSharpBehaviour>(true).Where(udon => IsExtention(udon.GetType()) && !IsDFUNC(udon.GetType()));
@@ -43,6 +43,91 @@ namespace EsnyaAircraftAssets
         public static IEnumerable<UdonSharpBehaviour> FindDFUNCs(GameObject root)
         {
             return root.GetUdonSharpComponentsInChildren<UdonSharpBehaviour>(true).Where(udon => IsDFUNC(udon.GetType()));
+        }
+
+        public static void SetObjectArrayProperty<T>(SerializedProperty property, IEnumerable<T> enumerable) where T : UnityEngine.Object
+        {
+            var array = enumerable.ToArray();
+            property.arraySize = array.Length;
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                property.GetArrayElementAtIndex(i).objectReferenceValue = array[i];
+            }
+        }
+
+        public static void UndoRecordUdonSharpBehaviour(UdonSharpBehaviour udonSharpBehaviour, string name)
+        {
+            Undo.RecordObject(udonSharpBehaviour, name);
+        }
+
+        public static bool ValidateReference<T>(UdonSharpBehaviour extention, string variableName, T expectedValue, MessageType messageType) where T : class
+        {
+            if (expectedValue == null || extention.GetProgramVariable(variableName) != null) return false;
+
+            if (ESFAUI.HelpBoxWithAutoFix($"{extention}.{variableName} is not set.", messageType))
+            {
+                UndoRecordUdonSharpBehaviour(extention, "Auto Fix");
+                extention.SetProgramVariable(variableName, expectedValue);
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void AlignMFDFunctions(this SaccEntity entity, VRC_Pickup.PickupHand side)
+        {
+            var parent = entity.InVehicleOnly?.transform ?? entity.transform;
+            var display = FindByName(parent, $"StickDisplay{side.ToString()[0]}")?.transform;
+            if (!display) return;
+
+            var functions = Enumerable
+                .Range(0, display.childCount)
+                .Select(display.GetChild)
+                .Where(t => t.gameObject.name.StartsWith("MFD_"))
+                .Select((transform, index) => (transform, index))
+                .ToArray();
+
+            var count = functions.Length;
+            var dialFunctions = side == VRC_Pickup.PickupHand.Left ? entity.Dial_Functions_L : entity.Dial_Functions_R;
+            foreach (var (transform, index) in functions)
+            {
+                var localRotation = Quaternion.AngleAxis(360.0f * index / count, Vector3.back);
+                var localPosition = localRotation * Vector3.up * 0.14f;
+
+                Undo.RecordObject(transform, "Align MFD Function");
+                transform.localPosition = localPosition;
+                transform.localScale = Vector3.one;
+
+                var dialFunction = dialFunctions != null && index < dialFunctions.Length ? dialFunctions[index] : null;
+
+                var displayHighlighter = transform.Find("MFD_display_funcon")?.gameObject;
+                if (displayHighlighter)
+                {
+                    Undo.RecordObject(displayHighlighter.transform, "Align MFD Function");
+                    displayHighlighter.transform.position = transform.parent.position;
+                    displayHighlighter.transform.localRotation = localRotation;
+
+                    if ((UnityEngine.Object)dialFunction.GetProgramVariable("Dial_Funcon") != displayHighlighter)
+                    {
+                        var udon = UdonSharpEditorUtility.GetBackingUdonBehaviour(dialFunction);
+                        Undo.RecordObject(udon, "Align MFD Function");
+                        dialFunction.SetProgramVariable("Dial_Funcon", displayHighlighter);
+                        dialFunction.ApplyProxyModifications();
+                        EditorUtility.SetDirty(udon);
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<GameObject> ListByName(this Transform root, string name)
+        {
+            return root.GetComponentsInChildren<Transform>(true).OrderBy(t => t.GetHierarchyPath().Count(c => c == '/')).Select(t => t.gameObject).Where(o => o.name == name);
+        }
+
+        public static GameObject FindByName(this Transform root, string name)
+        {
+            return ListByName(root, name).FirstOrDefault();
         }
     }
 }

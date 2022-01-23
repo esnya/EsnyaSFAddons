00c2a9fb0da3623b7e3513396886a427a9af42d3
esnya <esnya@nekometer.com>
2022-01-23 22:44:22 +0900

    :bug: fix: Fix windsock

--- Assets/EsnyaSFAddons/Scripts/Weather/Windsock.cs
+++ Assets/EsnyaSFAddons/Scripts/Weather/Windsock.cs
@@ -12,13 +12,13 @@ namespace EsnyaAircraftAssets
     public class Windsock : UdonSharpBehaviour
     {
         public SAV_WindChanger windChanger;
-        public int updateInterval = 30;
+        public int updateInterval = 10;
         public float maxWindMagnitude = 100.0f;
-        public float rotationSmooth = 100.0f;
+        public float rotationSmooth = 1f;
 
         private int updateOffset;
         private Animator animator;
-        private float heading;
+        private float heading, targetHeading;
         private Quaternion initialRotation;
 
         private void Start()
@@ -31,23 +31,27 @@ namespace EsnyaAircraftAssets
 
         private void Update()
         {
-            if ((Time.frameCount + updateOffset) % updateInterval != 0 || windChanger == null) return;
+            var prevHeading = heading;
+            heading = Mathf.LerpAngle(heading + 180, targetHeading, Time.deltaTime / rotationSmooth);
+            if (!Mathf.Approximately(heading, prevHeading)) transform.rotation = Quaternion.AngleAxis(heading + 180, Vector3.up) * initialRotation;
 
-            var wind = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).Wind;
-            var windGustiness = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindGustiness;
-            var windTurbulanceScale = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindTurbulanceScale;
-            var windGustStrength = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindGustStrength;
+            if ((Time.frameCount + updateOffset) % updateInterval == 0 && windChanger)
+            {
+                var wind = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).Wind;
+                var windGustiness = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindGustiness;
+                var windTurbulanceScale = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindTurbulanceScale;
+                var windGustStrength = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindGustStrength;
 
-            var timeGustiness = Time.time * windGustiness;
-            var gustx = timeGustiness + (transform.position.x * windTurbulanceScale);
-            var gustz = timeGustiness + (transform.position.z * windTurbulanceScale);
-            var finalWind = wind + Vector3.Normalize(new Vector3(Mathf.PerlinNoise(gustx + 9000, gustz) - .5f, 0, Mathf.PerlinNoise(gustx, gustz + 9999) - .5f)) * windGustStrength;
+                var timeGustiness = Time.time * windGustiness;
+                var gustx = timeGustiness + (transform.position.x * windTurbulanceScale);
+                var gustz = timeGustiness + (transform.position.z * windTurbulanceScale);
+                var finalWind = wind + Vector3.Normalize(new Vector3(Mathf.PerlinNoise(gustx + 9000, gustz) - .5f, 0, Mathf.PerlinNoise(gustx, gustz + 9999) - .5f)) * windGustStrength;
 
-            heading = Mathf.LerpAngle(heading, Vector3.SignedAngle(Vector3.forward, Vector3.ProjectOnPlane(finalWind, Vector3.up), Vector3.up), rotationSmooth * Mathf.Clamp01(finalWind.magnitude) / (Time.deltaTime * updateInterval));
-            transform.rotation = Quaternion.AngleAxis(heading, Vector3.up) * initialRotation;
+                targetHeading = Vector3.SignedAngle(Vector3.forward, Vector3.ProjectOnPlane(finalWind, Vector3.up), Vector3.up);
 
-            animator.SetFloat("x", Mathf.Clamp(finalWind.x / maxWindMagnitude, -1.0f, 1.0f));
-            animator.SetFloat("z", Mathf.Clamp(finalWind.z / maxWindMagnitude, -1.0f, 1.0f));
+                animator.SetFloat("x", Mathf.Clamp(finalWind.x / maxWindMagnitude, -1.0f, 1.0f));
+                animator.SetFloat("z", Mathf.Clamp(finalWind.z / maxWindMagnitude, -1.0f, 1.0f));
+            }
         }
     }
 }

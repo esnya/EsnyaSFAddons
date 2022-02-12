e6c6eea11e99a3acfcacc4632359a8cd3bd4c500
esnya <esnya@nekometer.com>
2022-02-12 17:31:19 +0900

    :sparkles: feat(editor): Add Gizmos for SaccEntity/SaccAirVehicle

--- Assets/EsnyaSFAddons/Editor/SVGizmoDrawer.cs
+++ Assets/EsnyaSFAddons/Editor/SVGizmoDrawer.cs
@@ -11,10 +11,25 @@ namespace EsnyaAircraftAssets
 {
     public class VehicleSeat
     {
-        public static class GizmoColors
+        private class GizmoColors
         {
-            public static readonly Color eyePosition = Color.white;
-            public static readonly Color floatPoint = Color.cyan;
+            public static Color eyePosition = Color.white;
+            public static Color floatPoint = Color.cyan;
+            public static Color centerOfMass = Color.white;
+            public static Color groundDetector = Color.white;
+            public static Color pitchMount = Color.white;
+            public static Color yawMount = Color.white;
+        }
+
+
+        private static void DrawSaccEntityGizmos(SaccEntity entity)
+        {
+            var centerOfMass = entity.CenterOfMass;
+            if (centerOfMass)
+            {
+                Gizmos.color = GizmoColors.centerOfMass;
+                Gizmos.DrawWireSphere(centerOfMass.position, 0.1f);
+            }
         }
 
         private static void DrawSaccVehicleSeatGizmos(SaccVehicleSeat saccVehicleSeat)
@@ -24,6 +39,37 @@ namespace EsnyaAircraftAssets
 
             Gizmos.color = GizmoColors.eyePosition;
             Gizmos.DrawWireSphere(targetEyePosition.position, 0.1f);
+        }
+
+        private static void DrawSaccAirVehicleGizmos(SaccAirVehicle airVehicle)
+        {
+            var entity = airVehicle.EntityControl;
+
+            var entityTransform = entity?.transform ?? airVehicle.transform;
+
+            var pitchMount = airVehicle.PitchMoment;
+            if (pitchMount)
+            {
+                Gizmos.color = GizmoColors.pitchMount;
+                Gizmos.DrawWireSphere(pitchMount.position, 0.1f);
+                Gizmos.DrawLine(pitchMount.position - entityTransform.up, pitchMount.position + entityTransform.up);
+            }
+
+            var yawMount = airVehicle.YawMoment;
+            if (yawMount)
+            {
+                Gizmos.color = GizmoColors.yawMount;
+                Gizmos.DrawWireSphere(yawMount.position, 0.1f);
+                Gizmos.DrawLine(yawMount.position - entityTransform.right, yawMount.position + entityTransform.right);
+            }
+
+            var groundDetector = airVehicle.GroundDetector;
+            if (groundDetector)
+            {
+                Gizmos.color = GizmoColors.groundDetector;
+                Gizmos.DrawWireSphere(groundDetector.position, 0.1f);
+                Gizmos.DrawRay(groundDetector.position, Vector3.down * airVehicle.GroundDetectorRayDistance);
+            }
         }
 
         private static void DrawSAV_FloatScriptGizmos(SAV_FloatScript floatScript)
@@ -40,14 +86,16 @@ namespace EsnyaAircraftAssets
             }
         }
 
-        [DrawGizmo(GizmoType.Selected | GizmoType.Active)]
+        [DrawGizmo(GizmoType.InSelectionHierarchy)]
         public static void SVGizmoDrawer(UdonBehaviour udonBehaviour, GizmoType gizmoType)
         {
             if (!UdonSharpEditorUtility.IsUdonSharpBehaviour(udonBehaviour)) return;
 
             var udonSharpBehaviour = UdonSharpEditorUtility.GetProxyBehaviour(udonBehaviour);
 
+            if (udonSharpBehaviour is SaccEntity) DrawSaccEntityGizmos(udonSharpBehaviour as SaccEntity);
             if (udonSharpBehaviour is SaccVehicleSeat) DrawSaccVehicleSeatGizmos(udonSharpBehaviour as SaccVehicleSeat);
+            if (udonSharpBehaviour is SaccAirVehicle) DrawSaccAirVehicleGizmos(udonSharpBehaviour as SaccAirVehicle);
             if (udonSharpBehaviour is SAV_FloatScript) DrawSAV_FloatScriptGizmos(udonSharpBehaviour as SAV_FloatScript);
         }
     }

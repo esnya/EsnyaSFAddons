58f7c1e4fe918d8c1acf366dfabb2a3953f2ec2a
esnya <esnya@nekometer.com>
2022-03-23 22:50:15 +0900

    :sparkles: feat(accesory): Ground Wind Indicator

---
+++ Assets/EsnyaSFAddons/Scripts/Accesories/GroundWindIndicator.cs
@@ -0,0 +1,46 @@
+﻿
+using TMPro;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace EsnyaSFAddons
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class GroundWindIndicator : UdonSharpBehaviour
+    {
+        public float updateInterval = 10.0f;
+
+        public SAV_WindChanger windChanger;
+        public Transform directionIndicator;
+        public TextMeshProUGUI speedText;
+
+
+        private void OnEnable()
+        {
+            SendCustomEventDelayedSeconds(nameof(_ThinUpdate), Random.Range(0, updateInterval));
+        }
+
+        public void _ThinUpdate()
+        {
+            if (!gameObject.activeInHierarchy || !windChanger) return;
+
+            var strength = windChanger.WindStrength * 1.944f;
+            var gust = windChanger.WindGustStrength * 1.944f;
+            var angle = Vector3.SignedAngle(windChanger.transform.forward, Vector3.forward, Vector3.up);
+
+            if (directionIndicator)
+            {
+                directionIndicator.localRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
+
+            if (speedText)
+            {
+                speedText.text = $"{strength:0}\n{strength + gust:0}";
+            }
+
+            SendCustomEventDelayedSeconds(nameof(_ThinUpdate), updateInterval);
+        }
+    }
+}

a409359d53f9d375a6a09ac6db213e4e9f26027b
esnya <esnya@nekometer.com>
2022-03-26 01:34:45 +0900

    :sparkles: feat(dfunc): BuddyPod

--- Assets/EsnyaSFAddons/Scripts/Accesories/DrogueHead.cs
+++ Assets/EsnyaSFAddons/Scripts/Accesories/DrogueHead.cs
@@ -1,6 +1,4 @@
-﻿
-using System.Net;
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
@@ -11,50 +9,24 @@ namespace EsnyaSFAddons
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class DrogueHead : UdonSharpBehaviour
     {
-        public float detatchDistance = 2.0f;
-        public AudioSource fuelingSound;
-        public AudioClip contactSound, detachSound;
-        public float udpateInterval = 1f;
-        public int resupplylayer = 27;
-        private SaccEntity target;
-        private Transform probe;
-
-        private void OnDisable()
-        {
-            target = null;
-            probe = null;
-            if (fuelingSound) fuelingSound.Stop();
-        }
+        public string prefix = "Probe";
+        public UdonSharpBehaviour eventTarget;
+        public string eventName = "_Contact";
+        [System.NonSerialized] public SaccEntity targetEntity;
+        [System.NonSerialized] public Collider probeCollider;
 
         private void OnTriggerEnter(Collider other)
         {
-            if (!other || !other.attachedRigidbody || other.gameObject.layer != resupplylayer) return;
+            if (!other || !other.attachedRigidbody || !other.gameObject.name.StartsWith(prefix)) return;
+            probeCollider = other;
 
             var vehicleRigidbody = other.attachedRigidbody;
             if (!Networking.IsOwner(vehicleRigidbody.gameObject)) return;
 
-            target = vehicleRigidbody.GetComponent<SaccEntity>();
-            if (!target) return;
-            probe = other.transform;
+            targetEntity= vehicleRigidbody.GetComponent<SaccEntity>();
+            if (!targetEntity) return;
 
-            if (contactSound && fuelingSound) fuelingSound.PlayOneShot(contactSound);
-            if (fuelingSound && !fuelingSound.isPlaying) fuelingSound.Play();
-
-            SendCustomEventDelayedSeconds(nameof(_Supply), udpateInterval);
-        }
-
-        public void _Supply()
-        {
-            if (!target || !probe || Vector3.Distance(transform.position, probe.position) > detatchDistance)
-            {
-                if (detachSound && fuelingSound) fuelingSound.PlayOneShot(detachSound);
-                if (fuelingSound) fuelingSound.Stop();
-            }
-            else
-            {
-                target.SendEventToExtensions("SFEXT_O_ReSupply");
-                SendCustomEventDelayedSeconds(nameof(_Supply), udpateInterval);
-            }
+            eventTarget.SendCustomEvent(eventName);
         }
     }
 }
---
+++ Assets/EsnyaSFAddons/Scripts/DFUNC/DFUNC_BuddyPod.cs
@@ -0,0 +1,230 @@
+using JetBrains.Annotations;
+using UdonSharp;
+using UnityEngine;
+
+namespace EsnyaSFAddons
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class DFUNC_BuddyPod : UdonSharpBehaviour
+    {
+        public GameObject Dial_Funcon;
+        public float extendDuration = 5;
+        public Transform drogueHead;
+        public int drogueIKLength = 2;
+        public int drogueLinearLength = 0;
+        [NotNull] public DrogueHead drogueHeadTrigger;
+        public float reSupplyInterval = 1;
+        public float contactMaxDistance = 1f;
+        public float contactMinDistance = 0.5f;
+        public float drogueRestAngle = 5f;
+        public float drogueRestDistance = 0.98f;
+
+        public AudioSource fuelingSound;
+        public AudioSource contactSound, detachSound;
+
+        private bool onGround = true;
+        [UdonSynced][FieldChangeCallback(nameof(Extend))] private bool _extend;
+        private bool Extend
+        {
+            set
+            {
+                _extend = value;
+                if (Dial_Funcon && Dial_Funcon.activeSelf != value) Dial_Funcon.SetActive(value);
+                if (drogueHeadTrigger.gameObject.activeSelf != value) drogueHeadTrigger.gameObject.SetActive(value);
+            }
+            get => _extend;
+        }
+
+        private string triggerAxis;
+        public void DFUNC_LeftDial()
+        {
+            triggerAxis = "Oculus_CrossPlatform_PrimaryIndexTrigger";
+        }
+        public void DFUNC_RightDial()
+        {
+            triggerAxis = "Oculus_CrossPlatform_SecondaryIndexTrigger";
+        }
+
+        private bool selected;
+        public void DFUNC_Selected()
+        {
+            selected = true;
+        }
+        public void DFUNC_Deselected()
+        {
+            selected = false;
+        }
+
+        public void KeyboardInput()
+        {
+            if (Extend || !onGround)
+            {
+                Extend = !Extend;
+                RequestSerialization();
+            }
+        }
+
+        private Transform[] drogueIKBones;
+        private Vector3[] drogueIKBonePositions;
+        private Transform drogueBase;
+        private Vector3 drogueOffset;
+        private float drogueLength;
+        private Vector3 drogueRestPosition;
+        public void SFEXT_L_EntityStart()
+        {
+            drogueHeadTrigger.transform.SetParent(drogueHead, false);
+
+            drogueIKBones = new Transform[drogueIKLength];
+            drogueIKBonePositions = new Vector3[drogueIKLength];
+            var currentBone = drogueHead.parent;
+            for (var i = 0; i < drogueIKLength; i++)
+            {
+                drogueIKBones[drogueIKLength - i - 1] = currentBone;
+                drogueIKBonePositions[drogueIKLength - i - 1] = currentBone.localPosition;
+                currentBone = currentBone.parent;
+            }
+            drogueBase = currentBone;
+            drogueLength = Vector3.Distance(drogueHead.position, drogueBase.position);
+            drogueOffset = drogueBase.InverseTransformVector(drogueHead.parent.position - drogueHead.position);
+            drogueRestPosition = Quaternion.AngleAxis(-drogueRestAngle, Vector3.right) * Vector3.up * drogueLength * drogueRestDistance;
+
+            Extend = false;
+
+            ResolveIK(0, Vector3.zero);
+
+            gameObject.SetActive(false);
+        }
+
+        private SaccEntity targetEntity;
+        private Transform targetProbe;
+        private bool hasPilot;
+        public void SFEXT_G_PilotEnter()
+        {
+            gameObject.SetActive(true);
+            drogueRestPosition = Quaternion.AngleAxis(-drogueRestAngle, Vector3.right) * Vector3.up * drogueLength * drogueRestDistance;
+            hasPilot = true;
+        }
+        public void SFEXT_G_PilotExit()
+        {
+            hasPilot = false;
+        }
+
+        public void SFEXT_G_TakeOff()
+        {
+            onGround = false;
+        }
+        public void SFEXT_G_TouchDown()
+        {
+            onGround = true;
+        }
+
+        private float extendingRatio;
+        private float prevReSupplyTime;
+        private void Update()
+        {
+            if (selected)
+            {
+                if (GetTriggerDown() && (Extend || !onGround))
+                {
+                    Extend = !Extend;
+                    RequestSerialization();
+                }
+            }
+            var extendTarget = Extend ? 1.0f : 0.0f;
+            var drogueMoving = !Mathf.Approximately(extendTarget, extendingRatio);
+            var retracted = Mathf.Approximately(extendingRatio, 0.0f);
+
+            if (drogueMoving) extendingRatio = Mathf.MoveTowards(extendingRatio, extendTarget, Time.deltaTime / extendDuration);
+
+            if (targetEntity)
+            {
+                var targetProbePosition = drogueBase.InverseTransformPoint(targetProbe.position) + drogueOffset;
+                var targetProbeDistance = targetProbePosition.magnitude / drogueLength;
+                if (targetProbeDistance > contactMaxDistance || targetProbeDistance < contactMinDistance)
+                {
+                    Detach();
+                    ResolveIK(extendingRatio, drogueRestPosition);
+                }
+                else
+                {
+                    ResolveIK(extendingRatio, targetProbePosition);
+                    var time = Time.time;
+                    if (time - prevReSupplyTime > reSupplyInterval)
+                    {
+                        prevReSupplyTime = time;
+                        targetEntity.SendEventToExtensions("SFEXT_O_ReSupply");
+                        Debug.Log("ReSupply");
+                    }
+                }
+            }
+            else if (drogueMoving)
+            {
+                ResolveIK(extendingRatio, drogueRestPosition);
+            }
+
+            if (retracted && !drogueMoving && !hasPilot)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+
+        public void _Contact()
+        {
+            if (targetEntity) return;
+            Contact(drogueHeadTrigger.targetEntity, drogueHeadTrigger.probeCollider.transform);
+        }
+
+        private void Contact(SaccEntity entity, Transform probe)
+        {
+            targetEntity = entity;
+            targetProbe = probe;
+            SetPlay(contactSound, true);
+            SetPlay(fuelingSound, true);
+        }
+
+        private void Detach()
+        {
+            targetEntity = null;
+            targetProbe = null;
+            SetPlay(detachSound, true);
+            SetPlay(fuelingSound, false);
+        }
+
+        private bool prevTrigger;
+        private bool GetTriggerDown()
+        {
+            var trigger = Input.GetAxisRaw(triggerAxis) > 0.75f;
+            var triggerDown = trigger && !prevTrigger;
+            prevTrigger = trigger;
+            return triggerDown;
+        }
+
+        private void ResolveIK(float extendingRatio, Vector3 drogueHeadPosition)
+        {
+            var distance = drogueHeadPosition.magnitude;
+            var xz = Vector3.ProjectOnPlane(drogueHeadPosition, Vector3.up) * extendingRatio;
+            var y = Vector3.Project(drogueHeadPosition, Vector3.up) * extendingRatio;
+            var d = Mathf.Sqrt(Mathf.Max(drogueLength - distance, 0.0f) * 3 * distance / 8) * extendingRatio * Vector3.forward;
+            var yScaler = 1.0f / (drogueIKLength - 1.0f);
+            var xzScaler = 1.0f / (drogueIKLength - drogueLinearLength - 1.0f);
+            for (var i = 0; i < drogueIKLength; i++)
+            {
+                var ypos = i * yScaler;
+                var xzpos = Mathf.Max(i - drogueLinearLength, 0) * xzScaler;
+                drogueIKBones[i].position = drogueBase.TransformPoint(ypos * y + xzpos * xz - Parabola1(xzpos) * d * xzpos);
+            }
+        }
+
+        private void SetPlay(AudioSource audioSource, bool play)
+        {
+            if (!audioSource || audioSource.isPlaying == play) return;
+            if (play) audioSource.Play();
+            else audioSource.Stop();
+        }
+
+        private float Parabola1(float x)
+        {
+            return 4.0f * x * (1.0f - x);
+        }
+    }
+}

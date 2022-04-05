25e6bffdbba16cc9d2dbada14f169d4c7159c1af
esnya <esnya@nekometer.com>
2022-04-05 14:06:20 +0900

    :sparkles: feat: Boarding Collider

--- Assets/EsnyaSFAddons/Editor/ESFAMenu.cs
+++ Assets/EsnyaSFAddons/Editor/ESFAMenu.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using UnityEditor;
+using UnityEngine;
 
 namespace EsnyaSFAddons
 {
@@ -23,6 +25,44 @@ namespace EsnyaSFAddons
             {
                 var syms = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
                 PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, syms.Replace(symbol, "").Replace(";;", ";"));
+            }
+        }
+
+        private static void SetLayerName(int layer, string name)
+        {
+            var tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset"));
+            tagManager.Update();
+
+            var layersProperty = tagManager.FindProperty("layers");
+            layersProperty.arraySize = Mathf.Max(layersProperty.arraySize, layer);
+            layersProperty.GetArrayElementAtIndex(layer).stringValue = name;
+
+            tagManager.ApplyModifiedProperties();
+        }
+
+        public static readonly int[] addonLayers = {
+            29,
+        };
+        public static readonly string[] addonLayerNames = {
+            "BoardingCollider"
+        };
+        public static readonly LayerMask[] addonLayerCollisions = {
+            0b0101_1111_1111_1101_1010_1111_1101_1111,
+        };
+
+        [MenuItem("SaccFlight/EsnyaSFAddons/Setup Addon Layers")]
+        public static void SetupAddonLayers()
+        {
+            var zippedAddonLayers = addonLayers
+                .Zip(addonLayerNames, (layer, name) => (layer, name))
+                .Zip(addonLayerCollisions, (t, collision) => (t.layer, t.name, collision));
+            foreach (var (layer, name, collision) in zippedAddonLayers)
+            {
+                SetLayerName(layer, name);
+                for (var i = 0; i < 32; i++)
+                {
+                    Physics.IgnoreLayerCollision(layer, i, ((1 << i) & collision) == 0);
+                }
             }
         }
 
---
+++ Assets/EsnyaSFAddons/Scripts/SFEXT/SFEXT_BoardingCollider.cs
@@ -0,0 +1,104 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+
+namespace EsnyaSFAddons
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class SFEXT_BoardingCollider : UdonSharpBehaviour
+    {
+        private Collider[] colliders;
+        private Quaternion localRotation;
+        private Transform entityTransform;
+        private Vector3 localPosition;
+
+        [UdonSynced][FieldChangeCallback(nameof(DoorsOpened))] private bool _doorsOpened;
+        private bool DoorsOpened
+        {
+            set
+            {
+                _doorsOpened = value;
+                UpdateCollidersEnabled();
+            }
+            get => _doorsOpened;
+        }
+        private bool _onBoarding;
+        private bool OnBoarding {
+            set {
+                _onBoarding = value;
+                UpdateCollidersEnabled();
+            }
+            get => _onBoarding;
+        }
+        private bool _collidersEnabled;
+        private bool CollidersEnabled {
+            set {
+                _collidersEnabled = value;
+                foreach (var collider in colliders) collider.enabled = value;
+            }
+            get => _collidersEnabled;
+        }
+
+        public void SFEXT_L_EntityStart()
+        {
+            colliders = GetComponentsInChildren<Collider>(true);
+
+            entityTransform = GetComponentInParent<SaccEntity>().transform;
+            localPosition = entityTransform.InverseTransformPoint(transform.position);
+            localRotation = Quaternion.Inverse(entityTransform.rotation) * transform.rotation;
+
+            transform.SetParent(entityTransform.parent, true);
+
+            gameObject.name = $"{entityTransform.gameObject.name}_{gameObject.name}";
+
+            DoorsOpened = true;
+            OnBoarding = false;
+        }
+
+        public void SFEXT_O_DoorsClosed()
+        {
+            DoorsOpened = false;
+            RequestSerialization();
+        }
+
+        public void SFEXT_O_DoorsOpened()
+        {
+            DoorsOpened = true;
+            RequestSerialization();
+        }
+
+        public void SFEXT_O_PilotEnter()
+        {
+            OnBoarding = true;
+        }
+        public void SFEXT_O_PilotExit()
+        {
+            OnBoarding = false;
+        }
+
+        public void SFEXT_P_PassengerEnter()
+        {
+            OnBoarding = true;
+        }
+        public void SFEXT_P_PassengerExit()
+        {
+            OnBoarding = false;
+        }
+
+        public override void PostLateUpdate()
+        {
+            if (!DoorsOpened) return;
+            transform.position = entityTransform.TransformPoint(localPosition);
+            transform.rotation = entityTransform.rotation * localRotation;
+        }
+
+        private void UpdateCollidersEnabled()
+        {
+            var value = DoorsOpened && !OnBoarding;
+            if (value != CollidersEnabled) CollidersEnabled = true;
+        }
+    }
+}

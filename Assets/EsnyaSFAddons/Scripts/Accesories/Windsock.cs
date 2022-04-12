03ce33d8cad0de2a842cb15144c9ed5779ee3ca6
esnya <esnya@nekometer.com>
2022-04-12 17:44:59 +0900

    :zap: perf(windsock): More improvment Windsock

--- Assets/EsnyaSFAddons/Scripts/Accesories/Windsock.cs
+++ Assets/EsnyaSFAddons/Scripts/Accesories/Windsock.cs
@@ -9,64 +9,93 @@ namespace EsnyaAircraftAssets
     public class Windsock : UdonSharpBehaviour
     {
         public SAV_WindChanger windChanger;
+        public Vector3 windsockAxis = Vector3.up;
         public int updateInterval = 10;
         public float maxWindMagnitude = 100.0f;
-        public float rotationSmooth = 1f;
-        public float rotationOffset = 180.0f;
+        public float rotationResponse =1f;
+        public float rotationOffset = 0.0f;
 
         public bool cloth = true;
-        public bool animation = false;
-        public bool negateClothWind = true;
+        public bool negateClothWind = false;
 
         private int updateOffset;
         private Animator animator;
-        private float heading, targetHeading;
-        private Quaternion initialRotation;
-        private float finalWindTarget, finalWindSmoothed;
+        private Vector3 wind;
+        private float windGustiness, windTurbulanceScale, windGustStrength;
+
+        private float _windHeading;
+        private float WindHeading
+        {
+            set
+            {
+                if (!Mathf.Approximately(WindHeading, value))
+                {
+                    transform.rotation = Quaternion.AngleAxis(value + rotationOffset, windsockAxis);
+                }
+                _windHeading = value;
+            }
+            get => _windHeading;
+        }
+
+
+        private Vector3 _finalWind;
+        private Vector3 FinalWind
+        {
+            get => _finalWind;
+            set
+            {
+                if (Vector3.Distance(value, _finalWind) > 0)
+                {
+                    if (cloth)
+                    {
+                        var sign = negateClothWind ? -1.0f : 1.0f;
+                        animator.SetFloat("x", Mathf.Clamp(value.x / maxWindMagnitude, -1.0f, 1.0f) * sign);
+                        animator.SetFloat("z", Mathf.Clamp(value.z / maxWindMagnitude, -1.0f, 1.0f) * sign);
+                    }
+                    else
+                    {
+                        animator.SetFloat("finalwind", value.magnitude / maxWindMagnitude);
+                    }
+                }
+                _finalWind = value;
+            }
+        }
 
         private void Start()
         {
             updateOffset = Random.Range(0, updateInterval);
             animator = GetComponent<Animator>();
-            initialRotation = transform.rotation;
-            heading = 0;
+            WindHeading = 0;
+            FinalWind = Vector3.zero;
         }
 
-        private void Update()
+        private void OnAnimatorMove()
         {
-            var prevHeading = heading;
-            heading = Mathf.LerpAngle(heading, targetHeading, Time.deltaTime / rotationSmooth);
-            if (!Mathf.Approximately(heading, prevHeading)) transform.rotation = Quaternion.AngleAxis(heading + rotationOffset, Vector3.up) * initialRotation;
+            if (!cloth) _Update();
+        }
 
-            if ((Time.frameCount + updateOffset) % updateInterval == 0 && windChanger)
+        private void OnRenderObject() => _Update();
+
+        private void _Update()
+        {
+            if (!windChanger) return;
+
+            if ((Time.frameCount + updateOffset) % updateInterval == 0)
             {
-                var wind = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).Wind;
-                var windGustiness = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindGustiness;
-                var windTurbulanceScale = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindTurbulanceScale;
-                var windGustStrength = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindGustStrength;
-
-                var timeGustiness = Time.time * windGustiness;
-                var gustx = timeGustiness + (transform.position.x * windTurbulanceScale);
-                var gustz = timeGustiness + (transform.position.z * windTurbulanceScale);
-                var finalWind = wind + Vector3.Normalize(new Vector3(Mathf.PerlinNoise(gustx + 9000, gustz) - .5f, 0, Mathf.PerlinNoise(gustx, gustz + 9999) - .5f)) * windGustStrength;
-
-                targetHeading = Vector3.SignedAngle(Vector3.back, Vector3.ProjectOnPlane(finalWind, Vector3.up), Vector3.up);
-
-                if (cloth)
-                {
-                    var sign = negateClothWind ? -1.0f : 1.0f;
-                    animator.SetFloat("x", Mathf.Clamp(finalWind.x / maxWindMagnitude, -1.0f, 1.0f) * sign);
-                    animator.SetFloat("z", Mathf.Clamp(finalWind.z / maxWindMagnitude, -1.0f, 1.0f) * sign);
-                }
-
-                finalWindTarget = finalWind.magnitude / maxWindMagnitude;
+                wind = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).Wind;
+                windGustiness = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindGustiness;
+                windTurbulanceScale = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindTurbulanceScale;
+                windGustStrength = ((SaccAirVehicle)windChanger.SaccAirVehicles[0]).WindGustStrength;
             }
 
-            if (animation && !Mathf.Approximately(finalWindSmoothed, finalWindTarget))
-            {
-                finalWindSmoothed = Mathf.LerpAngle(finalWindTarget, finalWindSmoothed, Time.deltaTime / rotationSmooth);
-                animator.SetFloat("finalwind", finalWindSmoothed);
-            }
+            var timeGustiness = Time.time * windGustiness;
+            var gustx = timeGustiness + (transform.position.x * windTurbulanceScale);
+            var gustz = timeGustiness + (transform.position.z * windTurbulanceScale);
+
+            FinalWind = wind + Vector3.Normalize(new Vector3(Mathf.PerlinNoise(gustx + 9000, gustz) - .5f, 0, Mathf.PerlinNoise(gustx, gustz + 9999) - .5f)) * windGustStrength;
+
+            var targetWindHeading = Vector3.SignedAngle(Vector3.back, FinalWind, Vector3.up);
+            WindHeading = Mathf.MoveTowardsAngle(WindHeading, targetWindHeading, Time.deltaTime * rotationResponse);
         }
     }
 }

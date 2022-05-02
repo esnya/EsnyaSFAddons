c69232ba4e4eddffd959ab2a1bb8e9e8269f73cf
esnya <2088693+esnya@users.noreply.github.com>
2022-05-03 00:45:28 +0900

    :sparkles: feat: Catapult Controller

---
+++ Assets/EsnyaSFAddons/Scripts/Accesories/CatapultController.cs
@@ -0,0 +1,45 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+
+namespace EsnyaSFAddons
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class CatapultController : UdonSharpBehaviour
+    {
+        public Transform catapultTrigger;
+        public float planeDetectionRadius = 2.0f;
+        public LayerMask planeDetectionLayerMask = 1 << 31 | 1 << 25 | 1 << 17;
+
+        public override void Interact()
+        {
+            var dfunc = FindDFunc();
+            if (!dfunc) return;
+            dfunc.SendCustomNetworkEvent(NetworkEventTarget.Owner, nameof(DFUNC_Catapult.KeyboardInput));
+        }
+
+        private DFUNC_Catapult FindDFunc()
+        {
+            foreach (var collider in Physics.OverlapSphere(catapultTrigger.position, planeDetectionRadius, planeDetectionLayerMask, QueryTriggerInteraction.Collide))
+            {
+                if (!collider) continue;
+
+                var rigidbody = collider.attachedRigidbody;
+                if (!rigidbody) continue;
+
+                var saccEntity = rigidbody.GetComponent<SaccEntity>();
+                if (!saccEntity) continue;
+
+                var dfunc = (DFUNC_Catapult)saccEntity.GetExtention(GetUdonTypeName<DFUNC_Catapult>());
+                if (!dfunc || !dfunc.OnCatapult) continue;
+
+                return dfunc;
+            }
+
+            return null;
+        }
+    }
+}

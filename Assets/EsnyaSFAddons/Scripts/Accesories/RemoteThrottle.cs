a685079461b16a26184e5bd2b466133e5ebcc6b0
esnya <2088693+esnya@users.noreply.github.com>
2022-05-11 16:22:38 +0900

    :sparkles: feat: Remote Throttle

---
+++ Assets/EsnyaSFAddons/Scripts/Accesories/RemoteThrottle.cs
@@ -0,0 +1,38 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace EsnyaSFAddons
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class RemoteThrottle : UdonSharpBehaviour
+    {
+        public float step = 0.1f;
+        private SaccAirVehicle airVehicle;
+
+        private void Start()
+        {
+            var saccEntity = GetComponentInParent<SaccEntity>();
+            airVehicle = (SaccAirVehicle)saccEntity.GetExtention(GetUdonTypeName<SaccAirVehicle>());
+            gameObject.SetActive(false);
+        }
+
+        public void Increase()
+        {
+            if (airVehicle && airVehicle.IsOwner)
+            {
+                airVehicle.PlayerThrottle = Mathf.Clamp01(airVehicle.PlayerThrottle + step);
+            }
+        }
+
+        public void Decrease()
+        {
+            if (airVehicle && airVehicle.IsOwner)
+            {
+                airVehicle.PlayerThrottle = Mathf.Clamp01(airVehicle.PlayerThrottle - step);
+            }
+        }
+    }
+}

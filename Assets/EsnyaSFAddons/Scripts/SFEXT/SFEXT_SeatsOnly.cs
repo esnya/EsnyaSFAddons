526492ef4b5f8f8a35801da850420ef1ea1df55b
esnya <2088693+esnya@users.noreply.github.com>
2022-05-16 20:48:23 +0900

    :sparkles: feat: Seats Only

---
+++ Assets/EsnyaSFAddons/Scripts/SFEXT/SFEXT_SeatsOnly.cs
@@ -0,0 +1,56 @@
+using UdonSharp;
+using UnityEngine;
+namespace EsnyaSFAddons
+{
+
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class SFEXT_SeatsOnly : UdonSharpBehaviour
+    {
+        public SaccVehicleSeat[] seats = { };
+        public bool excludeMode;
+        public GameObject[] objects = { };
+
+        public void SFEXT_L_EntityStart()
+        {
+            SetActive(false);
+        }
+
+        public void SFEXT_O_PilotEnter() => OnEnter();
+        public void SFEXT_O_PilotExit() => OnExit();
+        public void SFEXT_P_PassengerEnter() => OnEnter();
+        public void SFEXT_P_PassengerExit() => OnExit();
+
+        private void OnEnter()
+        {
+            foreach (var seat in seats)
+            {
+                if (seat && seat.EntityControl.MySeat == seat.ThisStationID)
+                {
+                    if (excludeMode) return;
+                    SetActive(true);
+                    break;
+                }
+            }
+
+            if (excludeMode) SetActive(true);
+        }
+
+        private void OnExit()
+        {
+            SetActive(false);
+        }
+
+        private void SetActive(bool value)
+        {
+            gameObject.SetActive(value);
+            if (objects != null)
+            {
+                foreach (var obj in objects)
+                {
+                    if (!obj) continue;
+                    obj.SetActive(value);
+                }
+            }
+        }
+    }
+}

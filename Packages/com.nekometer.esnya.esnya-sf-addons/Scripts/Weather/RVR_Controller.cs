337600c23224f51b84793beaca717523794596d4
esnya <2088693+esnya@users.noreply.github.com>
2022-06-16 17:46:34 +0900

    :sparkles: feat(rvr-controller): Auto find skybox material

--- Packages/com.nekometer.esnya.esnya-sf-addons/Scripts/Weather/RVR_Controller.cs
+++ Packages/com.nekometer.esnya.esnya-sf-addons/Scripts/Weather/RVR_Controller.cs
@@ -1,30 +1,62 @@
 ﻿
 using UdonSharp;
+using UdonToolkit;
 using UnityEngine;
 using VRC.SDK3.Components;
 using VRC.SDKBase;
-using VRC.Udon;
 
 namespace EsnyaSFAddons
 {
+    /// <summary>
+    /// Controls scene fog and skybox parameters.
+    ///
+    /// Attach to pickup object as handle like controller.
+    /// </summary>
     [RequireComponent(typeof(VRCPickup))]
     [UdonBehaviourSyncMode(BehaviourSyncMode.Continuous)]
     public class RVR_Controller : UdonSharpBehaviour
     {
         [Header("Scale")]
+        /// <summary>
+        /// Max moving distance of handle.
+        /// </summary>
         public float handleDistance = 0.5f;
 
         [Header("Fog Control")]
+        /// <summary>
+        /// Curve of fog value.
+        ///
+        /// At t = 0 with no fog, at t = 1 with maximum fog.
+        /// </summary>
         public AnimationCurve fogCurve = AnimationCurve.Linear(0, 0, 1, 1);
 
         [Header("Material Control (Skybox)")]
-        public Material material;
-        public string parameterName;
-        public AnimationCurve parameterCurve = AnimationCurve.Linear(0, 0, 1, 1);
+
+        /// <summary>
+        /// Control skybox material parameter.
+        /// </summary>
+        public bool controlSkybox = false;
+
+        /// <summary>
+        /// Name of material parameter
+        /// </summary>
+        [HideIf("@!controlSkybox")] public string parameterName;
+
+        /// <summary>
+        /// Curve of parameter value.
+        ///
+        /// At t = 0 with no fog, at t = 1 with maximum fog.
+        /// </summary>
+        [HideIf("@!controlSkybox")] public AnimationCurve parameterCurve = AnimationCurve.Linear(0, 0, 1, 1);
 
         private VRCPickup pickup;
         private Quaternion localRotation;
+        private Material skyboxMaterial;
 
+        [Header("Initial Values")]
+        /// <summary>
+        /// Initial value of fog density.
+        /// </summary>
         [UdonSynced(UdonSyncMode.Smooth)][FieldChangeCallback(nameof(Value))] public float _value;
         private float Value
         {
@@ -34,7 +66,7 @@ namespace EsnyaSFAddons
                 if (!Networking.IsOwner(gameObject)) SetHandlePosition();
 
                 RenderSettings.fogDensity = fogCurve.Evaluate(_value);
-                if (material) material.SetFloat(parameterName, parameterCurve.Evaluate(_value));
+                if (skyboxMaterial) skyboxMaterial.SetFloat(parameterName, parameterCurve.Evaluate(_value));
             }
             get => _value;
         }
@@ -43,6 +75,7 @@ namespace EsnyaSFAddons
         {
             localRotation = transform.localRotation;
             pickup = (VRCPickup)GetComponent(typeof(VRCPickup));
+            skyboxMaterial = controlSkybox ? RenderSettings.skybox : null;
             SetHandlePosition();
         }
 

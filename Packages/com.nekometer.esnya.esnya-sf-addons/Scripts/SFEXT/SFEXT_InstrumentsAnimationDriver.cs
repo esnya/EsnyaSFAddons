4b2407e4a2683a8f4854fae0909f3558459f9ef2
esnya <2088693+esnya@users.noreply.github.com>
2022-06-20 15:41:14 +0900

    :zap: perf(sfext-instruments-animation-driver): Improve Slip Indicator

--- Packages/com.nekometer.esnya.esnya-sf-addons/Scripts/SFEXT/SFEXT_InstrumentsAnimationDriver.cs
+++ Packages/com.nekometer.esnya.esnya-sf-addons/Scripts/SFEXT/SFEXT_InstrumentsAnimationDriver.cs
@@ -4,59 +4,218 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 using UdonToolkit;
+using JetBrains.Annotations;
+using System.Runtime.CompilerServices;
 
 namespace EsnyaSFAddons.SFEXT
 {
+    /// <summary>
+    /// Drive animation parameters of flight instruments.
+    /// </summary>
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class SFEXT_InstrumentsAnimationDriver : UdonSharpBehaviour
     {
-        public Animator instrumentsAnimator;
+        /// <summary>
+        /// Animator to drive.
+        ///
+        /// Use airVehicle.VehicleAnimator if null
+        /// </summary>
+        [CanBeNull] public Animator instrumentsAnimator;
 
+        /// <summary>
+        /// Response of vacuum power witch driven by engine
+        /// </summary>
         public float vacuumPowerResponse = 1.0f;
-        public GameObject batteryBus;
+
+        /// <summary>
+        /// Battery bus state.
+        ///
+        /// Always on if null.
+        /// </summary>
+        [CanBeNull] public GameObject batteryBus;
+
+        /// <summary>
+        /// Response of battery voltage.
+        /// </summary>
         public float batteryVoltageResponse = 1.0f;
+
+        /// <summary>
+        /// Magnetic declination.
+        /// </summary>
         public float magneticDeclination;
-        private float velocityResponse = 0.25f;
+
+        /// <summary>
+        /// Response of velocity indicators.
+        /// </summary>
+        public float smoothedVelocityResponse = 0.25f;
 
         [Header("ADI")]
+        /// <summary>
+        /// Set true to enable Attitude Indicator.
+        /// </summary>
         public bool hasADI = true;
+
+        /// <summary>
+        /// Is electirc driven.
+        /// </summary>
         [HideIf("@!hasADI")] public bool adiElectric = false;
+
+        /// <summary>
+        /// Max pitch angle in degrees.
+        /// </summary>
         [HideIf("@!hasADI")] public float maxPitch = 30;
-        [HideIf("@!hasADI")] public string pithFloatParameter = "pitch";
-        [HideIf("@!hasADI")] public string rollFloatParameter = "roll";
+
+        /// <summary>
+        /// Name of parameter in animator.
+        ///
+        /// -maxPitch to maxPitch will be remapped to 0.0 to 1.0.
+        /// </summary>
+        [NotNull][HideIf("@!hasADI")] public string pithFloatParameter = "pitch";
+
+
+        /// <summary>
+        /// Name of parameter in animator.
+        ///
+        /// -180 to 180 degrees will be remapped to 0.0 to 1.0.
+        /// </summary>
+        [NotNull][HideIf("@!hasADI")] public string rollFloatParameter = "roll";
 
         [Header("HI")]
+        /// <summary>
+        /// Set true to enable Heading Indicator.
+        /// </summary>
         public bool hasHI = true;
-        [HideIf("@!hasHI")] public bool hiElectric = false;
-        [HideIf("@!hasHI")] public string headingFloatParameter = "heading";
+
+        /// <summary>
+        /// Is electirc driven.
+        /// </summary>
+        [NotNull][HideIf("@!hasHI")] public bool hiElectric = false;
+
+        /// <summary>
+        /// Name of parameter in animator.
+        ///
+        /// 0 to 360 degrees will be remapped to 0.0 to 1.0.
+        /// </summary>
+        [NotNull][HideIf("@!hasHI")] public string headingFloatParameter = "heading";
 
         [Header("ASI")]
+        /// <summary>
+        /// Set true to enable Airspeed Indicator.
+        /// </summary>
         public bool hasASI = true;
+
+        /// <summary>
+        /// Max indicated airspeed in knots.
+        /// </summary>
         [HideIf("@!hasASI")] public float maxAirspeed = 180.0f;
-        [HideIf("@!hasASI")] public string airspeedFloatParameter = "airspeed";
+
+        /// <summary>
+        /// Response of airspeed indicator.
+        /// </summary>
         [HideIf("@!hasASI")] public float asiResponse = 0.25f;
 
+        /// <summary>
+        /// Name of parameter in animator.
+        ///
+        /// 0 to maxAirspeed will be remapped to 0.0 to 1.0.
+        /// </summary>
+        [NotNull][HideIf("@!hasASI")] public string airspeedFloatParameter = "airspeed";
+
         [Header("Altimeter")]
+        /// <summary>
+        /// Set true to enable barometric Altimeter.
+        /// </summary>
         public bool hasAltimeter = true;
+        /// <summary>
+        /// Max indicated altitude in feet.
+        /// </summary>
         [HideIf("@!hasAltimeter")] public float maxAltitude = 20000;
-        [HideIf("@!hasAltimeter")] public string altitudeFloatParameter = "altitude";
+
+        /// <summary>
+        /// Response of altimeter.
+        /// </summary>
         [HideIf("@!hasAltimeter")] public float altimeterResponse = 0.25f;
 
+        /// <summary>
+        /// Name of parameter in animator.
+        ///
+        /// 0 to maxAltitude will be remapped to 0.0 to 1.0.
+        /// </summary>
+        [NotNull][HideIf("@!hasAltimeter")] public string altitudeFloatParameter = "altitude";
+
         [Header("TC")]
+        /// <summary>
+        /// Set true to enable Turn Coordinator
+        /// </summary>
         public bool hasTC = true;
+
+        /// <summary>
+        /// Is turn coordinator electric.
+        /// </summary>
         [HideIf("@!hasTC")] public bool tcElectric = true;
+
+        /// <summary>
+        /// Max turn rate.
+        /// </summary>
         [HideIf("@!hasTC")] public float maxTurn = 360.0f / 60.0f * 2.0f;
-        [HideIf("@!hasTC")] public float maxSlip = 12.0f;
-        [HideIf("@!hasTC")] public string turnRateFloatParameter = "turnrate";
-        [HideIf("@!hasTC")] public string slipAngleFloatParameter = "slipangle";
+
+        /// <summary>
+        /// Response of turn coordinator.
+        /// </summary>
         [HideIf("@!hasTC")] public float turnResponse = 1.0f;
-        [HideIf("@!hasTC")] public float slipResponse = 1.0f;
+
+        /// <summary>
+        /// Name of parameter in animator.
+        ///
+        /// -maxTurn to maxTurn will be remapped to 0.0 to 1.0.
+        /// </summary>
+        [NotNull][HideIf("@!hasTC")] public string turnRateFloatParameter = "turnrate";
+
+        [Header("SI")]
+        /// <summary>
+        /// Set true to enable Slip Indicator ("Ball").
+        /// </summary>
+        public bool hasSI = true;
+
+        /// <summary>
+        /// Max slip angle in degrees.
+        /// </summary>
+        [HideIf("@!hasSI")] public float maxSlip = 12.0f;
+
+        /// <summary>
+        /// Response of slip indicator.
+        /// </summary>
+        [HideIf("@!hasSI")] public float slipResponse = 0.2f;
+
+        /// <summary>
+        /// Name of parameter in animator.
+        ///
+        /// -maxSlip to maxSlip will be remapped to 0.0 to 1.0.
+        /// </summary>
+        [NotNull][HideIf("@!hasSI")] public string slipAngleFloatParameter = "slipangle";
 
         [Header("VSI")]
+        /// <summary>
+        /// Set true to enable Vertical Speed Indicator.
+        /// </summary>
         public bool hasVSI = true;
+
+        /// <summary>
+        /// Max indicated vertical speed in feet per minute.
+        /// </summary>
         [HideIf("@!hasVSI")] public float maxVerticalSpeed = 2000;
-        [HideIf("@!hasVSI")] public string verticalSpeedFloatParameter = "vs";
+
+        /// <summary>
+        /// Response of vertical speed indicator.
+        /// </summary>
         [HideIf("@!hasVSI")] public float vsiResponse = 0.25f;
+
+        /// <summary>
+        /// Name of parameter in animator.
+        ///
+        /// -maxVerticalSpeed to maxVerticalSpeed will be remapped to 0.0 to 1.0.
+        /// </summary>
+        [NotNull][HideIf("@!hasVSI")] public string verticalSpeedFloatParameter = "vs";
 
         private SaccAirVehicle airVehicle;
         private Rigidbody vehicleRigidbody;
@@ -72,9 +231,13 @@ namespace EsnyaSFAddons.SFEXT
         private float roll;
         private float heading;
         private Vector3 velocity;
+        private Vector3 acceleration;
+        private Vector3 smoothedVelocity;
         private float prevRoll;
         private float prevHeading;
         private Vector3 prevVelocity;
+        private Vector3 forward;
+        private Vector3 up;
 
         private bool Battery
         {
@@ -124,13 +287,17 @@ namespace EsnyaSFAddons.SFEXT
         {
             if (!initaialized) return;
 
-            var forward = transform.forward;
+            forward = transform.forward;
+            up = transform.up;
 
             position = transform.position;
             deltaTime = Time.deltaTime;
-            roll = Vector3.SignedAngle(transform.up, Vector3.ProjectOnPlane(Vector3.up, forward).normalized, forward);
+            roll = Vector3.SignedAngle(up, Vector3.ProjectOnPlane(Vector3.up, forward).normalized, forward);
             heading = transform.eulerAngles.y;
-            velocity = Vector3.Lerp(velocity, (position - prevPosition) / deltaTime, deltaTime * velocityResponse);
+            velocity = (position - prevPosition) / deltaTime;
+            acceleration = (velocity - prevVelocity) / deltaTime;
+
+            smoothedVelocity = Vector3.Lerp(smoothedVelocity, velocity, deltaTime * smoothedVelocityResponse);
 
             var batteryVoltageTarget = Battery ? 1.0f : 0.0f;
             var batteryVoltageUpdate = !Mathf.Approximately(batteryVoltageTarget, batteryVoltage);
@@ -145,6 +312,7 @@ namespace EsnyaSFAddons.SFEXT
             if (hasASI) ASI_Update();
             if (hasAltimeter) Altimeter_Update();
             if (hasTC) TC_Update(tcElectric ? batteryVoltage : vacuumPower);
+            if (hasSI) SI_Update();
             if (hasVSI) VSI_Update();
 
             prevPosition = position;
@@ -179,7 +347,7 @@ namespace EsnyaSFAddons.SFEXT
             var gustz = TimeGustiness + (position.z * airVehicle.WindTurbulanceScale);
             var finalWind = (airVehicle.Wind + Vector3.Normalize(new Vector3(Mathf.PerlinNoise(gustx + 9000, gustz) - .5f, /* (Mathf.PerlinNoise(gustx - 9000, gustz - 9000) - .5f) */0, Mathf.PerlinNoise(gustx, gustz + 9999) - .5f)) * airVehicle.WindGustStrength) * airVehicle.Atmosphere;;
 
-            var airspeed = Mathf.Max(Vector3.Dot(velocity - finalWind, transform.forward), 0);
+            var airspeed = Mathf.Max(Vector3.Dot(smoothedVelocity - finalWind, forward), 0);
             instrumentsAnimator.SetFloat(airspeedFloatParameter, airspeed * 1.9438445f / maxAirspeed);
         }
 
@@ -191,20 +359,19 @@ namespace EsnyaSFAddons.SFEXT
 
         private void TC_Update(float power)
         {
-            var forward = transform.forward;
-
-            var acceleration = (velocity - prevVelocity) / deltaTime;
-
             turnRate = Mathf.Lerp(turnRate, (Mathf.DeltaAngle(heading, prevHeading) + Mathf.DeltaAngle(roll, prevRoll) * 0.5f) / deltaTime, deltaTime * turnResponse);
-            slipAngle = Mathf.Lerp(slipAngle, Mathf.Clamp(Vector3.SignedAngle(-transform.up, Vector3.ProjectOnPlane(Physics.gravity - acceleration, forward), forward), -maxSlip, maxSlip), deltaTime * slipResponse);
-
             instrumentsAnimator.SetFloat(turnRateFloatParameter, Remap01(turnRate, -maxTurn, maxTurn) * power);
+        }
+
+        private void SI_Update()
+        {
+            slipAngle = Mathf.Lerp(slipAngle, Mathf.Clamp(Vector3.SignedAngle(-up, Vector3.ProjectOnPlane(Physics.gravity - acceleration, forward), forward), -maxSlip, maxSlip), deltaTime * slipResponse);
             instrumentsAnimator.SetFloat(slipAngleFloatParameter, Remap01(slipAngle, -maxSlip, maxSlip));
         }
 
         private void VSI_Update()
         {
-            var verticalSpeed = velocity.y * 3.28084f * 60;
+            var verticalSpeed = smoothedVelocity.y * 3.28084f * 60;
             instrumentsAnimator.SetFloat(verticalSpeedFloatParameter, Remap01(verticalSpeed, -maxVerticalSpeed, maxVerticalSpeed));
         }
 
